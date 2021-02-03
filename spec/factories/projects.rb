f9879cb4267caec292dca402fddac859e9a591f9
kashinotti <ryo.ka.notti@gmail.com>
2021-02-03 15:11:31 +0000

    [finish]　ファクトリ内の重複をなくす

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,5 +4,22 @@ FactoryBot.define do
     description "A test project"
     due_on 1.week.from_now
     association :owner
+  
+
+
+    # 昨日が締め切りのプロジェクト
+    trait :due_yesterday do
+      due_on 1.day.ago
+    end
+  
+    # 今日が締め切りのプロジェクト
+    trait :due_today do
+      due_on Date.current.in_time_zone
+    end
+  
+    # 明日が締め切りのプロジェクト
+    trait :due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 end
